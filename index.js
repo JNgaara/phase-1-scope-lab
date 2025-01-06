0d49627df9aaeee9a9552866b239fb6cb2b81862
Joshua Ngaara <joshua.ngaara@student.moringaschool.com>
2025-01-06 02:07:10 +0200

    scpe

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+var customerName = 'bob';
+//var bestCustomer ;
+const leastFavoriteCustomer = 'kevin';
+function upperCaseCustomerName(){
+
+    customerName = customerName.toUpperCase();
+    return customerName ;
+}
+
+function setBestCustomer(){
+     bestCustomer = 'not bob';
+    return bestCustomer;
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+    return bestCustomer;
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'marc';
+    return leastFavoriteCustomer;
+    
+}
+customerName.upperCaseCustomerName();
+bestCustomer.setBestCustomer();
